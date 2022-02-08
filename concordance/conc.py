4de1d8866728cdf0a83f916b3cd4062a4a64491c
ClaudiaECarroll <95948642+ClaudiaECarroll@users.noreply.github.com>
2022-02-08 15:32:45 -0500

    Add files via upload

---
+++ concordance/conc.py
@@ -0,0 +1,53 @@
+import sys
+import os
+import io
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+
+files_path = sys.argv[1]
+textfile_dictionary = sys.argv[2]
+
+rmsa_words = ["face", "countenance", "manner", "look", "expression", "appearance"]
+
+for filename in os.listdir(files_path):
+    if filename.endswith(".txt"):
+
+        file = open(os.path.join(files_path, filename), "rt")
+        text = file.read()      
+
+        words = word_tokenize(text)
+
+        words = [word.lower() for word in words if word.isalpha()]
+
+        stops = stopwords.words("english")
+
+        tokens = [word for word in words if word not in stops]
+        tokens.insert(0, "pad")
+
+        conc_words = []
+        i=[]
+               
+        for word in rmsa_words:        
+            i += [x for x, token in enumerate(tokens) if token == word]
+        for number in i:
+                conc_words = conc_words + [tokens[number-1], tokens[number+1], tokens[number+2]]
+        print(conc_words)
+            
+                
+
+        file = io.open(textfile_dictionary, mode="r", encoding="utf8")
+        dictionaryread = file.read()
+        dictionary = dictionaryread.split()
+
+        rmsa_count = 0
+
+        for element in dictionary:
+            for w in conc_words:
+                if w == element:
+                    rmsa_count = rmsa_count + 1
+
+        print(filename, len(tokens), rmsa_count) 
+
+        
+            
+    
